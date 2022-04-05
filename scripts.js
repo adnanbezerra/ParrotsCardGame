14986bab389e724c8a469586fd0c0738ee71b6b7
adnan <adnanbezerra@gmail.com>
2022-04-05 12:35:08 -0300

    fim do layout básico

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let numeroCartas;
+
+let invalid = true;
+// while(invalid) {
+//     numeroCartas = Number(prompt("Com quantas cartas você quer jogar?"))
+
+//     if(numeroCartas % 2 === 0 && 4 <= numeroCartas && numeroCartas <= 14) break;
+// }
+
